d4ccb4049d416e27d7dc1f03822401ac797c8484
Matheus Carvalho <carvalho.bfr@gmail.com>
2026-08-11 16:51:38 +0200

    export validation core

--- src/index.ts
+++ src/index.ts
@@ -63,3 +63,7 @@ export * from "./security/repository-context.js";
 export * from "./storage/environment-credential-source.js";
 export * from "./storage/in-memory-store.js";
 export * from "./storage/json-file-store.js";
+export * from "./domain/validation.js";
+export * from "./validation/contract-parser.js";
+export * from "./validation/git-change-set.js";
+export * from "./validation/super-validator.js";
